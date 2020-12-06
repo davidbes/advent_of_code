8ba761632aa93d2d1bf2d3ea55a637ea2814f08d
David Bester <david.projects.01@gmail.com>
2020-12-06 08:40:07 +0100

    add oneliners

--- day_6.py
+++ day_6.py
@@ -1,15 +1,30 @@
-import collections
 data = " ".join(["-" if x == "\n" else x.strip()
                  for x in open("inputs/day_6.txt")]).split(" - ")
 
-overall_count = 0
-for group in data:
-    answers = group.replace(" ", "")
-    group_answers = collections.defaultdict(int)
-    for c in answers:
-        group_answers[c] += 1
 
-    for value in group_answers.values():
-        overall_count += value == len(group.split(" "))
+def task_one(polls) -> int:
+    count = 0
+    for group in polls:
+        unique = {x for x in group if x != " "}
+        count += len(unique)
+    return count
 
-print(overall_count)
+
+def task_two(polls) -> int:
+    count = 0
+    for group in polls:
+        unique = {x for x in group if x != " "}
+        for c in unique:
+            count += group.count(c) == len(group.split(" "))
+    return count
+
+
+def task_one_oneliner(polls) -> int:
+    return sum([len(unique) for group in polls for unique in {x for x in group if x != " "}])
+
+
+def task_two_oneliner(polls) -> int:
+    return sum([group.count(c) == len(group.split(" ")) for group in polls for c in {x for x in group if x != " "}])
+
+
+print(task_one_oneliner(data), task_two_oneliner(data))
