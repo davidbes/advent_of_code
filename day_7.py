5a603f97b0f4986758172dc9e92d90a88970385c
David Bester <david.projects.01@gmail.com>
2020-12-07 11:39:00 +0100

    add 2nd solution

--- day_7.py
+++ day_7.py
@@ -27,32 +27,30 @@ gold_bag = "shinygold"
 print(len(contains_bag(gold_bag, rules)))
 
 
-# def process_weight(rules):
-#     processed = dict()
-#     for rule in rules:
-#         splitted_rule = rule.split(" bags contain")
-#         rule_nm = "".join([x for x in splitted_rule[0].split(' ')])
-#         rulies = set()
-#         for rule in splitted_rule[1].split(','):
-#             short_rule = rule.replace("bags", "").replace(
-#                 "bag", "").replace(".", "").strip().split(" ")
-#             rule_tup = (int(short_rule[0]), "".join(short_rule[1:])) if len(
-#                 short_rule) == 3 else (0, "".join(short_rule))
-#             rulies.add(rule_tup)
-#         processed[rule_nm] = rulies
-#     return processed
+def process_weight(rules):
+    processed = dict()
+    for rule in rules:
+        splitted_rule = rule.split(" bags contain")
+        rule_nm = "".join([x for x in splitted_rule[0].split(' ')])
+        rulies = set()
+        for rule in splitted_rule[1].split(','):
+            short_rule = rule.replace("bags", "").replace(
+                "bag", "").replace(".", "").strip().split(" ")
+            rule_tup = (int(short_rule[0]), "".join(short_rule[1:])) if len(
+                short_rule) == 3 else (0, "".join(short_rule))
+            rulies.add(rule_tup)
+        processed[rule_nm] = rulies
+    return processed
 
 
-# def get_amount_of_baggs(bag, rules):
-#     amount = 0
-#     for item, bags in rules.items():
-#         for count, baggie in bags:
-#             summa = 1
-#             if bag == baggie:
-#                 if count != 0:
-#                     summa = summa + count * get_amount_of_baggs(item, rules)
-#             amount += summa - 1
-#     return amount
+def get_amount_of_baggs(bag, rules):
+    amount = 0
+    for count, rule in rules[bag]:
+        if count != 0:
+            amount += count
+            amount += count * get_amount_of_baggs(rule, rules)
+
+    return amount
 
 
-# print(get_amount_of_baggs(gold_bag, process_weight(data)))
+print(get_amount_of_baggs(gold_bag, process_weight(data)))
