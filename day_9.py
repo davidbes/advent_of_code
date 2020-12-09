5e3f8591ad8c1e9d2c676db3d2935b722825462e
David Bester <david.projects.01@gmail.com>
2020-12-09 15:36:41 +0100

    make day_9 10x faster

--- day_9.py
+++ day_9.py
@@ -1,4 +1,5 @@
 import time
+
 data = [int(x) for x in open("inputs/day_9.txt")]
 
 
@@ -16,19 +17,34 @@ def find_imposter(data, preamble):
             return number
 
 
-def find_weakness(data, desired_sum):
+# def find_weakness(data, desired_sum):  # AVG 0.234sec
+#     for i, _ in enumerate(data):
+#         numbers = []
+#         current_index = i
+#         while sum(numbers) < desired_sum:
+#             numbers.append(data[current_index])
+#             current_index += 1
+#         if sum(numbers) == desired_sum and len(numbers) >= 3:
+#             return max(numbers) + min(numbers)
+
+
+def find_weakness(data, desired_sum):   # AVG 0,024sec
     for i, num in enumerate(data):
-        numbers = []
+        num_sum = 0
         current_index = i
-        while sum(numbers) < desired_sum:
-            numbers.append(data[current_index])
+        curr_min, curr_max = num, num
+        while num_sum < desired_sum:
+            number = data[current_index]
+            num_sum += number
+            curr_max = curr_max if curr_max >= number else number
+            curr_min = curr_min if curr_min <= number else number
             current_index += 1
-        if sum(numbers) == desired_sum and len(numbers) >= 3:
-            return max(numbers) + min(numbers)
+        if num_sum == desired_sum and current_index - i >= 2:
+            return curr_min + curr_max
 
 
 timer = time.time()
 imposter = find_imposter(data, 25)
-print(imposter)
+
 print(find_weakness(data, imposter))
 print(time.time() - timer)
